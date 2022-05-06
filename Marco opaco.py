ef4c22675e0fab85b3c461f69d0c4ccf8ad35030
dianapaulinor <102988860+dianapaulinor@users.noreply.github.com>
2022-05-06 09:03:59 -0400

    Create Marco opaco.py

---
+++ Marco opaco.py
@@ -0,0 +1,8 @@
+Rect(0, 0, 400, 50, relleno='rojo', opacidad=50)
+Rect(0, 0, 50, 400, relleno='rojo', opacidad=50)
+Rect(350, 0, 50, 400, relleno='rojo', opacidad=50)
+Rect(0, 350, 400, 50, relleno='rojo', opacidad=50)
+Rect(25, 0, 100, 400, relleno='azul', opacidad=50)
+Rect(275, 0, 100, 400, relleno='azul', opacidad=50)
+Rect(0, 25, 400, 100, relleno='azul', opacidad=50)
+Rect(0, 275, 400, 100, relleno='azul', opacidad=50)
